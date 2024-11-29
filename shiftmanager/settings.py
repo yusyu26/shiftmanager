30f1ebcc66c468874a126cf816c4dff577c9678e
yusyu26 <yukisakurai929@gmail.com>
2024-11-29 21:38:59 +0900

    Update database configuration for Heroku

--- shiftmanager/settings.py
+++ shiftmanager/settings.py
@@ -130,3 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
