45cf1b6a5c762d2dea2f00e6c75da48b79c4529e
yusyu26 <yukisakurai929@gmail.com>
2024-11-30 12:20:06 +0900

    add runtime.txt

--- shiftmanager/settings.py
+++ shiftmanager/settings.py
@@ -131,4 +131,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
