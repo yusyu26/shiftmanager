b00cf03bfe9b89a2050544151a3b9dd0e4b756ca
yusyu26 <yukisakurai929@gmail.com>
2025-01-25 11:35:27 +0900

    認証機能の実装

--- shiftmanager/settings.py
+++ shiftmanager/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- shifts/views.py
+++ shifts/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 from .forms import PostForm
 from django.http import Http404
 from django.utils import timezone
 from .utils.line_notify import send_line_notification
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-    
 class Index(ListView):
     model = Post
     template_name = 'shifts/post_list.html'
@@ -32,8 +36,8 @@ class Index(ListView):
 class Detail (DetailView):
     model = Post
 
-from django.views.generic.edit import CreateView, UpdateView
-class Create(CreateView):
+
+class Create(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     success_url = "/"
@@ -47,7 +51,7 @@ class Create(CreateView):
         
         return response
 
-class Confirmsubstitute(UpdateView):
+class Confirmsubstitute(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['substitute']
     success_url = "/"
@@ -63,3 +67,31 @@ class Confirmsubstitute(UpdateView):
     def form_valid(self, form):
         # フォームが有効な場合の処理
         return super().form_valid(form)
+    
+def signupfunc(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username, '', password)
+            return redirect('login')
+        except IntegrityError:
+            return render(request, 'shifts/signup.html',{'error':'このユーザーは既に登録されています。'})
+    return render(request, 'shifts/signup.html',{'some':100})
+
+def loginfunc(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'shifts/login.html',{'context':'登録情報が正しくありません'})
+    return render(request, 'shifts/login.html',{}) 
+    
+def logoutfunc(request):
+    logout(request)
+    return redirect('/')
+    
